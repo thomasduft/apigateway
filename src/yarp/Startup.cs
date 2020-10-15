6b3fc3fe3a478ab1fc12321afb7e738cd18e1d2d
thomasduft <thomas.duft@gmail.com>
2020-10-15 10:16:23 +0200

    added yarp (wip)

---
+++ src/yarp/Startup.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiGateway.Proxy
+{
+  public class Startup
+  {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddReverseProxy()
+        .LoadFromConfig(Configuration.GetSection("ReverseProxy"));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseRouting();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapReverseProxy();
+      });
+    }
+  }
+}
